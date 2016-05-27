4b32eeaa5c6d3d80f39638482970a5c7ba145257
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-05-27 15:14:29 -0500

    push notifications

--- Daimler-Android/app/src/main/java/com/developers/devworms/daimler_android/Ingresar.java
+++ Daimler-Android/app/src/main/java/com/developers/devworms/daimler_android/Ingresar.java
@@ -1,21 +1,79 @@
 package com.developers.devworms.daimler_android;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.developers.devworms.daimler_android.mobile.AWSMobileClient;
+import com.developers.devworms.daimler_android.mobile.user.IdentityManager;
+
 public class Ingresar extends AppCompatActivity {
+    /** The identity manager used to keep track of the current user account. */
+    private IdentityManager identityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar);
+        // Obtain a reference to the mobile client. It is created in the Application class,
+        // but in case a custom Application class is not used, we initialize it here if necessary.
+        AWSMobileClient.initializeMobileClientIfNecessary(this);
+
+        // Obtain a reference to the mobile client. It is created in the Application class.
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // Obtain a reference to the identity manager.
+        identityManager = awsMobileClient.getIdentityManager();
+
+        setContentView(R.layout.activity_main);
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        final AWSMobileClient awsMobileClient = AWSMobileClient.defaultMobileClient();
+
+        // register notification receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(notificationReceiver,
+                new IntentFilter(PushListenerService.ACTION_SNS_NOTIFICATION));
+    }
+
+    private final BroadcastReceiver notificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Bundle data = intent.getBundleExtra(PushListenerService.INTENT_SNS_NOTIFICATION_DATA);
+            String message = PushListenerService.getMessage(data);
+
+            new AlertDialog.Builder(Ingresar.this)
+                    .setTitle(R.string.push_demo_title)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // unregister notification receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(notificationReceiver);
+    }
+
 
     public void llamarRegistro(View view) {
         Intent llamarScreenRegistro = new Intent(this, MainActivity.class);
